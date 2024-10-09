df30f7b2bfd62b0d683efff1c682bf5a9fb897f4
Brayan <brayanninja71@gmail.com>
2024-10-09 22:10:17 +0300

    db.json created, students data are captured

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -3,6 +3,7 @@ import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import { toast, ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css"
+import axios from 'axios';
 
 function Create (){
     const [ data, setData] = useState({
@@ -21,32 +22,49 @@ function Create (){
           [name]: value
         }))
       }
+      const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:4000/students', data)
+          .then(res => {
+            toast.success('New blog added successfully!', {
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 3000,
+            })
+          })
+          .catch(err => {
+            toast.error('An error occurred while adding the blog', {
+              position: toast.POSITION.TOP_RIGHT,
+              autoClose: 3000,
+            })
+          })
+      }
       return(
         <div>
-            <Form>
+            <Form onSubmit={handleSubmit}>
              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Name</Form.Label>
-              <Form.Control  name="name" type="name" placeholder="Enter both names" />
+              <Form.Control  name="name" type="name" onChange={handleChange}  placeholder="Enter both names" />
              </Form.Group>
              <Form.Select aria-label="Gender Select">
              <Form.Label>Gender</Form.Label>
-             <option value="">Choose Gender</option>  {/* Empty option for default selection */}
+             <option value="" onChange={handleChange} >Choose Gender</option>  {/* Empty option for default selection */}
              <option value="1">Male</option>
              <option value="2">Female</option>
              <option value="3">Others</option>
              </Form.Select>
              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Course</Form.Label>
-              <Form.Control name="course" type="text" placeholder="enter current course" />
+              <Form.Control name="course" type="text" onChange={handleChange}  placeholder="enter current course" />
              </Form.Group>
              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
               <Form.Label>Email address</Form.Label>
-              <Form.Control  name="email" type="text" placeholder="name@example.com" />
+              <Form.Control  name="email" type="text"  onChange={handleChange}  placeholder="name@example.com" />
              </Form.Group>
              <Form.Label htmlFor="inputPassword5">Password</Form.Label>
               <Form.Control
                 type="password"
                 name="password"
+                onChange={handleChange} 
                 id="inputPassword5"
                 aria-describedby="passwordHelpBlock"
              />
